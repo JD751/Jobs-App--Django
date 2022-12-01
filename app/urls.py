3f2e77ec58923a3e8044aef9f01c2ae6d293ed4b
Jawad Ali <jawad751@gmail.com>
2022-12-01 18:08:04 +0100

    url_and_views

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from app.views import hello, jdp
+from app.views import  jdp, job_list
 
 
 urlpatterns = [
-    path('', hello),
-    path('job/1', jdp)
+    path('', job_list),
+    path('job/<int:id>', jdp)  # creating dynamic url #int : path converter
+    #path converters could be more appropriately called as url path convereters
+
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+job_title = [
+
+    "First Job",
+    "Second Job",
+    "Third Job"
+]
+
+job_description = [
+    "First Job description",
+    "Second Job description",
+    "Third Job Description"
+
+
+]
 # Create your views here.
+
+
 def hello(request):
     return HttpResponse('Hello World! Live_url!')
 
-def jdp(request):
-    return HttpResponse('Job Detail Page')
+
+def jdp(request, id):
+    if id == 0:
+        return redirect("/")
+
+    print(type(id))
+    return_html = f"<h1> {job_title[id]}</h1> <h3> {job_description[id]}</h3>"
+    return HttpResponse(return_html)
+
+
+def job_list(request):
+    list_of_jobs = "<ul>"
+    for jobs in job_title:
+        job_id=job_title.index(jobs)
+        list_of_jobs += f"<li><a href= job/{job_id}>{jobs}</a></li>"
+    list_of_jobs +="</ul>"
+    return HttpResponse(list_of_jobs)
