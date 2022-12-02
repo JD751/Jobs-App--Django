74295ff98bf643e83ebc22660930e044df156a70
Jawad Ali <jawad751@gmail.com>
2022-12-02 17:19:35 +0100

    Django_templates

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from app.views import  jdp, job_list
+from app.views import  jdp, job_list,hello
 
 
 urlpatterns = [
-    path('', job_list),
-    path('job/<int:id>', jdp)  # creating dynamic url #int : path converter
+    path('', job_list, name= 'jobs_home'),
+    path('job/<int:id>', jdp, name='jobs_des'),  # creating dynamic url #int : path converter
     #path converters could be more appropriately called as url path convereters
-
+    path('hello/', hello, name='hello') 
 ]
+#use name spacing i-e under the templates folder have another folder with the app name
--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+from django.urls import reverse
+from django.template import loader
 
 job_title = [
 
@@ -24,17 +26,38 @@ def hello(request):
 
 def jdp(request, id):
     if id == 0:
-        return redirect("/")
+        return redirect(reverse('jobs_home'))
 
-    print(type(id))
-    return_html = f"<h1> {job_title[id]}</h1> <h3> {job_description[id]}</h3>"
-    return HttpResponse(return_html)
+    try:
+        return_html = f"<h1> {job_title[id]}</h1> <h3> {job_description[id]}</h3>"
+        return HttpResponse(return_html)
+    except:
+        return HttpResponseNotFound("Not Found")
+
+
+class TempClass:
+    x = 5
+    age = 35
 
 
 def job_list(request):
     list_of_jobs = "<ul>"
     for jobs in job_title:
-        job_id=job_title.index(jobs)
-        list_of_jobs += f"<li><a href= job/{job_id}>{jobs}</a></li>"
-    list_of_jobs +="</ul>"
+        job_id = job_title.index(jobs)
+        detail_url = reverse('jobs_des', args=(job_id,))
+        print(detail_url)
+        list_of_jobs += f"<li><a href= {detail_url}>{jobs}</a></li>"
+    list_of_jobs += "</ul>"
     return HttpResponse(list_of_jobs)
+
+
+def hello(request):
+    #template = loader.get_template('app/hello.html')
+    my_list = ["alpha", "beta"]
+    temp = TempClass()
+    context = {"name": "Jawad",
+               "first_list": my_list,
+               "t": temp
+               }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'app/hello.html', context)
